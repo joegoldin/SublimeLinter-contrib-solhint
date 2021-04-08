001924596ec8c7e09653f78f61eb2afb58c5a43a
Joe Goldin <joe@joegold.in>
2021-04-08 15:00:26 -0700

    Update linter.py

--- linter.py
+++ linter.py
@@ -6,7 +6,7 @@ from SublimeLinter.lint import NodeLinter, util
 class Solhint(NodeLinter):
     """Solhint class delegate call to solhint tool and return result back."""
 
-    cmd = 'solhint -f stylish '
+    cmd = 'solhint --config ./.solhint.json "contracts/**/*.sol"'
     regex = r'^\s+(?P<line>\d+):(?P<col>\d+)\s+((?P<error>error)|(?P<warning>warning))\s+(?P<message>.+)'
     multiline = False
     line_col_base = (1, 1)
